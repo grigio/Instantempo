f46272970825a2f3933406b61f9f77523a89a17d
Instantempo Dev Team <instantempo@instantempo.heroku.com>
2010-11-02 16:31:29 +0100

    instantempo first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instantempo::Application.config.secret_token = '728863f8b034ca9d75d68833438b53971aadb57f993144cb3edcaf0d0ca893f9af65a3466bcdd5bf51814052d84e74c65adebe075db2a33dfdcbe19af18301ab'
